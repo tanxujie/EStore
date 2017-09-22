9e34a85df88ae05e89aa29cf705cc674fafe6de0
01435337 <tanxujie@haier.com>
2017-09-22 23:42:42 +0800

    modify edit, delete button & function

--- src/main/resources/static/js/appjs/microclass.js
+++ src/main/resources/static/js/appjs/microclass.js
@@ -1,11 +1,3 @@
-function toEdit(id) {
-    window.location = './addmicroclass.html?id=' + id;
-}
-
-function removeConfirm(id) {
-    alert(id);
-}
-
 $(function(){
     // menu
     $('.ui.menu .ui.dropdown').dropdown({on: 'hover'});
@@ -16,7 +8,7 @@ $(function(){
     // 初始化查询表格
     var $table = $('#resultsTbl').DataTable({
         'serverSide': false,
-        'select':true,
+        //'select':true,
         'paging': true,
         'bPaginate': true,
         'iDisplayLength': 10,
@@ -36,15 +28,19 @@ $(function(){
             { 'title': '序号', 'target': 0, 'width': '5%', 'data': 'id'},
             { 'title': '标题', 'target': 1, 'width': '20%', 'data': 'title'},
             { 'title': '说明', 'target': 2, 'sortable':false, 'width': '40%', 'data': 'description' }
-//            { 'title': '', 'target': 3, 'data': function(item){
-//                var id = item.id;
-//                var str = '<button class="ui teal mini button" onclick="toEdit('+id+');"><i class="edit icon"></i>编辑</button>';
-//                str += '<button class="ui grey mini button" onclick="removeConfirm('+id+');"><i class="delete icon"></i>删除</button>';
-//                return str;
-//            }}
         ]
     });
-    
+
+    $('#resultsTbl tbody').on( 'click', 'tr', function () {
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            $table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    } );
+
     $("#btnAdd").click(function(event){
         event.preventDefault();
         event.stopPropagation();
@@ -54,7 +50,12 @@ $(function(){
     $("#btnEdit").click(function(event){
         event.preventDefault();
         event.stopPropagation();
-        window.location = './addmicroclass.html?id=';
+        var ids = $.map($table.rows('.selected').data(), function (item) {
+            return item['id'];
+        });
+        if (ids && ids.length) {
+            window.location = './editmicroclass.html?id='+ids[0];
+        }
     });
 
     $("#btnDelete").click(function(event){
